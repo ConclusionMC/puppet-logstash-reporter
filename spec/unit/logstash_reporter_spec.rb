a12a2681bb75799c118f47fc4964cf6b129559b7
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2020-02-05 15:20:29 +0100

    lint fixes

--- spec/unit/logstash_reporter_spec.rb
+++ spec/unit/logstash_reporter_spec.rb
@@ -11,7 +11,6 @@ processor = Puppet::Reports.report(:logstash)
 
 describe processor do
   describe "#process" do
-
     after :each do
         @server.close
     end
@@ -19,11 +18,9 @@ describe processor do
     before :each do
       @server = TCPServer.new 5999
       @report = YAML.load_file(File.join(fixture_path, 'report2.6.x.yaml')).extend processor
-
     end
 
-    it  "should send the right data to the remote server" do
-
+    it "should send the right data to the remote server" do
       @report.process
       client = @server.accept()
       resp = client.read()
@@ -31,10 +28,7 @@ describe processor do
       expect(data).not_to be_empty
       expect(data['@version']).to eq(1)
       expect(data['@timestamp']).to eq(Time.now.utc.iso8601)
-
       expect(data).to include("environment", "report_format", "puppet_version", "status", "start_time", "end_time", "tags")
-
-
     end
 
     it "rejects invalid hostnames" do
